3861985a603f283a83dc19b639edfbbc82af3221
synodical <lena0323park@naver.com>
2022-02-26 23:07:37 +0900

    Database 세팅 - mysql, sequelize

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const flash = require('connect-flash');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
+const { sequelize } = require('./models');
 
 const app = express();
 app.set('port', process.env.PORT || 8001);
@@ -17,6 +18,13 @@ nunjucks.configure('views', {
   express: app,
   watch: true,
 });
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ models/resident.js
@@ -0,0 +1,17 @@
+//const { DataTypes } = require("sequelize/types");
+//const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => (
+    sequelize.define('resident', {
+        room: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        name: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+        },
+    }, {
+        timestamps: false,
+    })
+);
