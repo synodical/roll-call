fd129a952e2fdfb96869311453e9da8079ddb85f
synodical <lena0323park@naver.com>
2022-02-27 15:27:52 +0900

    Fix error when User.findOne is not a function

--- passport/index.js
+++ passport/index.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const local = require('./localStrategy');
 //const kakao = require('./kakaoStrategy');
-const User = require('../models/user');
+const User = require('../models').User; 
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-const User = require('../models/user');
+const User = require('../models').User; 
 
 const router = express.Router();
 
