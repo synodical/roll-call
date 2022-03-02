f4f28543b09ff60871bc4e813a862387777fdfe0
synodical <lena0323park@naver.com>
2022-03-02 22:56:19 +0900

    Feat: user 정보 update 가능하게 하기

--- routes/page.js
+++ routes/page.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-
+const User = require('../models').User;
 const router = express.Router();
 router.get('/user/:id', isLoggedIn, (req, res) => {
     res.render('profile', { title: '내 정보 수정', user: req.user });
 });
-router.put('/user/:id', isLoggedIn, (req, res) => {
-    return data.update({
-        email: req.body.email,
-        nick: req.body.nick,
-        floor: req.body.floor,
-    }).then(function (data) {
-        if (data) {
-            res.send(data)
-        } else {
-            res.status(400).send('Error')
+
+router.post('/user/:id', isLoggedIn, (req, res) => {
+    User.update({ email: req.body.email, nick: req.body.nick, floor: req.body.floor }, {
+        where: {
+            id: req.params.id
         }
+    }).then(result => {
+        return res.redirect('/');
+    }).catch(err => {
+        console.log(err);
     })
 });
+
 router.get('/join', isNotLoggedIn, (req, res) => {
     res.render('join', {
         title: '회원가입',
@@ -25,6 +25,7 @@ router.get('/join', isNotLoggedIn, (req, res) => {
         joinError: req.flash('joinError'),
     });
 });
+
 router.get('/', (req, res, next) => {
     res.render('main', {
         titie: '점호',
