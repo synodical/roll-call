dbe9d3bf1876e38279b84e88c629d867525c61f4
synodical <lena0323park@naver.com>
2022-03-23 21:18:36 +0900

    resident router 삭제

--- routes/user.js
+++ routes/user.js
@@ -20,5 +20,43 @@ router.post('/:id', isLoggedIn, (req, res) => {
   })
 });
 
+router.get('/:id/list', isLoggedIn, async (req, res, next) => {
+  try {
+    const residents = await Resident.findAll({
+      order: [['room', 'ASC']],
+      include: {
+        model: User,
+        where: { uid: req.user.uid },
+      },
+    });
+    console.log(residents);
+    res.render('list', {
+      title: '사생 명단',
+      user: req.user,
+      residents: residents,
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+router.post('/:id/list', isLoggedIn, async (req, res, next) => {
+  const { room, name } = req.body;
+  //console.log(req.params.id);
+  var id = req.params.id;
+  try {
+    await Resident.create({
+      room: room,
+      name: name,
+      user_id: req.user.id,
+    });
+    return res.redirect(`/user/${id}/list`);
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
+});
+
 
 module.exports = router;
