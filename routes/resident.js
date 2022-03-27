23fa080a1e933e73783dac0f25c1ee6e9fd17ac1
synodical <lena0323park@naver.com>
2022-03-27 16:49:26 +0900

    prettier 적용

--- routes/resident.js
+++ routes/resident.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-const User = require('../models').User;
-const Resident = require('../models').Resident;
+const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+const User = require("../models").User;
+const Resident = require("../models").Resident;
 const router = express.Router();
 
-router.get('/:id/list', isLoggedIn, async (req, res, next) => {
+router.get("/:id/list", isLoggedIn, async (req, res, next) => {
   try {
     const residents = await Resident.findAll({
-      order: [['room', 'ASC']],
+      order: [["room", "ASC"]],
       include: {
         model: User,
         where: { uid: req.user.uid },
       },
     });
     console.log(residents);
-    res.render('list', {
-      title: '사생 명단',
+    res.render("list", {
+      title: "사생 명단",
       user: req.user,
       residents: residents,
     });
@@ -25,7 +25,7 @@ router.get('/:id/list', isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.post('/:id/list', isLoggedIn, async (req, res, next) => {
+router.post("/:id/list", isLoggedIn, async (req, res, next) => {
   const { room, name } = req.body;
   var id = req.params.id;
   try {
@@ -41,14 +41,13 @@ router.post('/:id/list', isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.route('/:id')
-  .delete(async (req, res, next) => {
-    try {
-      const result = await Resident.destroy({ where: { id: req.params.id } });
-      res.json(result)
-    } catch (err) {
-      console.log(err);
-      next(err)
-    }
-  })
+router.route("/:id").delete(async (req, res, next) => {
+  try {
+    const result = await Resident.destroy({ where: { id: req.params.id } });
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
 module.exports = router;
