8c08c2536536c4a47986b6335e119eee3d79a41d
synodical <lena0323park@naver.com>
2022-03-28 00:18:07 +0900

    delRow, getCheckboxCnt 함수 main.js 분리

--- public/main.js
+++ public/main.js
@@ -7,14 +7,22 @@ async function delRow(row, obj) {
     console.error(err);
   }
 }
-function getCheckboxCnt(event) {
+
+function getInCheckboxCnt(event) {
   // 선택된 목록 가져오기
   const query = 'input[name="in"]:checked';
   const selectedElements = document.querySelectorAll(query);
-
   // 선택된 목록의 갯수 세기
   const selectedElementsCnt = selectedElements.length;
-
   // 출력
-  document.getElementById("result").innerText = selectedElementsCnt;
+  console.log(selectedElementsCnt);
+  document.getElementById("resultIn").innerText = selectedElementsCnt;
+}
+
+function getOutCheckboxCnt(event) {
+  const query = 'input[name="out"]:checked';
+  const selectedElements = document.querySelectorAll(query);
+  const selectedElementsCnt = selectedElements.length;
+
+  document.getElementById("resultOut").innerText = selectedElementsCnt;
 }
