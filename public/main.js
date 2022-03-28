4d5fd2ae9c26aa7d629f6b9eeb39782e512b8d01
synodical <lena0323park@naver.com>
2022-03-28 18:41:49 +0900

    getCheckboxCnt 함수 중복 제거

--- public/main.js
+++ public/main.js
@@ -8,21 +8,15 @@ async function delRow(row, obj) {
   }
 }
 
-function getInCheckboxCnt(event) {
+function getCheckboxCnt(event, name) {
   // 선택된 목록 가져오기
-  const query = 'input[name="in"]:checked';
+  console.log(name);
+  const query = `input[name="${name}"]:checked`;
+  console.log(query);
   const selectedElements = document.querySelectorAll(query);
   // 선택된 목록의 갯수 세기
   const selectedElementsCnt = selectedElements.length;
   // 출력
   console.log(selectedElementsCnt);
-  document.getElementById("resultIn").innerText = selectedElementsCnt;
-}
-
-function getOutCheckboxCnt(event) {
-  const query = 'input[name="out"]:checked';
-  const selectedElements = document.querySelectorAll(query);
-  const selectedElementsCnt = selectedElements.length;
-
-  document.getElementById("resultOut").innerText = selectedElementsCnt;
+  document.getElementById(`result-${name}`).innerText = selectedElementsCnt;
 }
