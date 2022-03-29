c1ece9de3e037bf4ce0122e60396eac16ba9a644
synodical <lena0323park@naver.com>
2022-03-29 12:19:38 +0900

    벌점 대상자 명단 보여주기

--- public/main.js
+++ public/main.js
@@ -8,15 +8,31 @@ async function delRow(row, obj) {
   }
 }
 
-function getCheckboxCnt(event, name) {
+function getCheckboxCnt(name) {
   // 선택된 목록 가져오기
-  console.log(name);
   const query = `input[name="${name}"]:checked`;
-  console.log(query);
   const selectedElements = document.querySelectorAll(query);
   // 선택된 목록의 갯수 세기
   const selectedElementsCnt = selectedElements.length;
   // 출력
-  console.log(selectedElementsCnt);
   document.getElementById(`result-${name}`).innerText = selectedElementsCnt;
+}
+
+function getResidents(name) {
+  const query = `input[name="${name}"]:checked`;
+  const selectedElements = document.querySelectorAll(query);
+  let result = "";
+
+  selectedElements.forEach((el, index) => {
+    let tr = el.parentNode.parentNode.parentNode;
+    let Rroom = tr.firstElementChild;
+    let Rname = tr.children[1];
+
+    result += Rroom.textContent + " " + Rname.textContent;
+    // 마지막 단어는 쉼표 없애기
+    if (index !== selectedElements.length - 1) {
+      result += ", ";
+    }
+  });
+  document.getElementById(`${name}-room`).innerText = result;
 }
