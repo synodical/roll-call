848f240d06e289d243d9f471df5b43480d4585ab
synodical <lena0323park@naver.com>
2022-04-05 00:15:44 +0900

    prettier 적용 및 rest api

--- app.js
+++ app.js
@@ -1,60 +1,62 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const path = require('path');
-const session = require('express-session');
-const nunjucks = require('nunjucks');
-const dotenv = require('dotenv');
-const flash = require('connect-flash');
-const passport = require('passport');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+const flash = require("connect-flash");
+const passport = require("passport");
 
 dotenv.config();
-const pageRouter = require('./routes/page');
-const authRouter = require('./routes/auth');
-const userRouter = require('./routes/user');
-const residentRouter = require('./routes/resident');
-const { sequelize } = require('./models');
-const passportConfig = require('./passport');
+const pageRouter = require("./routes/page");
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
+const residentRouter = require("./routes/resident");
+const { sequelize } = require("./models");
+const passportConfig = require("./passport");
 
 const app = express();
 passportConfig(); // 패스포트 설정
-app.set('port', process.env.PORT || 8001);
-app.set('view engine', 'ejs');
-nunjucks.configure('views', {
+app.set("port", process.env.PORT || 8001);
+app.set("view engine", "ejs");
+nunjucks.configure("views", {
   express: app,
   watch: true,
 });
-sequelize.sync({ force: false })
+sequelize
+  .sync({ force: false })
   .then(() => {
-    console.log('데이터베이스 연결 성공');
+    console.log("데이터베이스 연결 성공");
   })
   .catch((err) => {
     console.error(err);
   });
 
-
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', pageRouter);
-app.use('/auth', authRouter);
-app.use('/user', userRouter);
-app.use('/resident', residentRouter);
+app.use("/", pageRouter);
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
+app.use("/residents", residentRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -64,11 +66,11 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
 });
