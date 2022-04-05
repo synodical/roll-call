1755418769aee6f4da392685fd9152f17bda9b1d
synodical <lena0323park@naver.com>
2022-04-05 20:42:01 +0900

    config.json 내의 변수 .env 로 옮기기

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
-const config = require("../config/config")[env];
+const config = require("../config/config.js")[env];
 
 const db = {};
 const sequelize = new Sequelize(
